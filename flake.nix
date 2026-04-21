2f22c0ee2c93c914a1ba2286947133a334495281
Sam B <samlingx@gmail.com>
2026-04-21 13:06:57 -0700

    Change folder name expectations

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       imports = [
         (import-tree.filterNot (lib.hasSuffix "hardware-configuration.nix") ./modules)
         (import-tree.filterNot (lib.hasSuffix "/package.nix") ./pkgs)
-      ] ++ lib.optional (builtins.pathExists /home/sboynton/nv-dotfiles/default.nix) /home/sboynton/nv-dotfiles/default.nix;
+      ] ++ lib.optional (builtins.pathExists /home/sboynton/work-dotfiles/default.nix) /home/sboynton/work-dotfiles/default.nix;
 
       options.flake.modules = lib.mkOption {
         type = lib.types.lazyAttrsOf (lib.types.lazyAttrsOf lib.types.deferredModule);
