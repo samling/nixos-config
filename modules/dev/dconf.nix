40810fc1b06cbd6212753ff95ff6681265f7fb6d
Sam B <samlingx@gmail.com>
2026-04-23 14:43:40 -0700

    Enable linger on user

--- modules/dev/dconf.nix
+++ modules/dev/dconf.nix
@@ -1,5 +1,10 @@
+{ config, ... }:
+let
+  owner = config.flake.meta.owner.username;
+in
 {
   flake.modules.nixos.dev = { ... }: {
     programs.dconf.enable = true;
+    users.users.${owner}.linger = true;
   };
 }
