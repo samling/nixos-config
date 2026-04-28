a12cbc02930cb934e0b3dfd6a6caa72d5de716bd
Sam Boynton <samlingx@gmail.com>
2026-04-27 21:10:39 -0700

    Upgrade flake inputs and packages

--- pkgs/toofan/package.nix
+++ pkgs/toofan/package.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "toofan";
-  version = "2.1.0";  # or a tag if there is one
+  version = "2.1.1";  # or a tag if there is one
 
   src = fetchFromGitHub {
     owner = "vyrx-dev";
     repo = "toofan";
     rev = "master";  # pin to a commit sha for reproducibility
-    hash = "sha256-oez6iev1sxel4dy4kAqRdhZstpttC1VMEysoP9QrcEc=";
+    hash = "sha256-JkU1UIXuV9pKE5Tjg7nlWdgJ2manTpqnHCJg3VPTR7A=";
   };
 
   vendorHash = "sha256-YSjJ8NOL97hXZLnfGYIjoKmARv+gWOsv+5qkl9konnA=";
